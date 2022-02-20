b551b5d0ce472bfd204ac1b308dd11d1dea8b0fa
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-20 15:15:35 +0000

    finish next solution

--- JQuery/HPlusSport_Start/solution.js
+++ JQuery/HPlusSport_Start/solution.js
@@ -1,5 +1,49 @@
-$("document").ready(function() {
-    $("h2[data-type=vitamin]").css({"background" : "green"})
-    $("h2[data-type=mineralwater]").css({"background" : "blue", "color" : "gold"})
-    $("h2[data-type=proteinbar]").css({"background" : "purple", "color" : "gold"})
+$('document').ready(function () {
+  $('h2[data-type=vitamin]').css({ background: 'green' });
+  $('h2[data-type=mineralwater]').css({ background: 'blue', color: 'gold' });
+  $('h2[data-type=proteinbar]').css({ background: 'purple', color: 'gold' });
+  // part 2
+  document
+    .querySelector('#cbVitamins')
+    .addEventListener('change', function (evt) {
+      updateProductView('vitamins', evt.target.checked);
+    });
+  document
+    .querySelector('#cbMineralWater')
+    .addEventListener('change', function (evt) {
+      updateProductView('mineralwater', evt.target.checked);
+    });
+  document
+    .querySelector('#cbProtein')
+    .addEventListener('change', function (evt) {
+      updateProductView('proteinbar', evt.target.checked);
+    });
+
+  $('.product-item').each(function () {
+    var prodName = encodeURIComponent($(this).children('h2').text());
+    var prodID = encodeURIComponent($(this).data('prod_id'));
+
+    var link = $(
+      "<a href='product.html?prodName=" + prodName + '&prodID=' + prodID + "'/>"
+    );
+    $(this).children('img').wrap(link);
+  });
 });
+
+function updateProductView(categoryName, bVisible) {
+  var dataSelectorVal = '';
+  switch (categoryName) {
+    case 'vitamins':
+      dataSelectorVal = "h2[data-type='vitamin']";
+      break;
+    case 'mineralwater':
+      dataSelectorVal = "h2[data-type='mineralwater']";
+      break;
+    case 'proteinbar':
+      dataSelectorVal = "h2[data-type='proteinbar']";
+      break;
+  }
+  $('.product-item')
+    .has(dataSelectorVal)
+    .css('display', bVisible ? '' : 'none');
+}
