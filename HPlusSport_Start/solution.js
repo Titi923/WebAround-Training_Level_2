eaa3b7734684cb7817cfbf19e8af58efe1541086
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-20 11:13:04 +0000

    finish my solution.

--- HPlusSport_Start/solution.js
+++ HPlusSport_Start/solution.js
@@ -1,3 +1,5 @@
 $("document").ready(function() {
-    
+    $("h2[data-type=vitamin]").css({"background" : "green"})
+    $("h2[data-type=mineralwater]").css({"background" : "blue", "color" : "gold"})
+    $("h2[data-type=proteinbar]").css({"background" : "purple", "color" : "gold"})
 });
