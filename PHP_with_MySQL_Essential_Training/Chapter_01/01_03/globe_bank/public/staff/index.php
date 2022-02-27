8a6e3d61f9c121f812f0ece70a4b00ae233a8ba0
Titi923 <petrisor.buciuta92@gmail.com>
2022-02-27 22:24:24 +0000

    add css to file

--- PHP_with_MySQL_Essential_Training/Chapter_01/01_03/globe_bank/public/staff/index.php
+++ PHP_with_MySQL_Essential_Training/Chapter_01/01_03/globe_bank/public/staff/index.php
@@ -1,12 +1,30 @@
 <!doctype html>
 
 <html lang="en">
-  <head>
-    <title></title>
-    <meta charset="utf-8">
-  </head>
 
-  <body>
+<head>
+  <title>GBI</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" media="all" href="../stylesheets/staff.css"/>
+</head>
 
-  </body>
+<body>
+  <header>
+    <h1>GBI Staff Area</h1>
+  </header>
+
+  <navigation>
+    <ul>
+      <li>
+        <a href="index.php">Menu</a>
+      </li>
+    </ul>
+  </navigation>
+
+  <footer>
+    &copy; <?=date("Y");?> Globe Bank
+  </footer>
+
+</body>
+
 </html>
